fec4aada0a52fcb37956c8c4cd68a10d397426c2
Tintin2710 <haiding.wang@etu.u-paris.fr>
2024-09-12 16:07:06 +0200

    add condtion for detecting helix

--- secondary_structure_assigner.py
+++ secondary_structure_assigner.py
@@ -100,7 +100,7 @@ class SecondaryStructureAssigner:
                     )
 
                     # Helix detection: Check φ and ψ angles and distance
-                    if diff in [3,4] and (-90 <= donor_phi <= -40 and -70 <= donor_psi <= -20) and (-90 <= acceptor_phi <= -40 and -70 <= acceptor_psi <= -20):
+                    if diff in [3,4,5] and (-90 <= donor_phi <= -40 and -70 <= donor_psi <= -20) and (-90 <= acceptor_phi <= -40 and -70 <= acceptor_psi <= -20):
                         helix.add(donor_residue_id)
                         helix.add(acceptor_residue_id)
 
